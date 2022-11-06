bb0856daba3f6021ec2b23fddfcd986e7dc65828
yhosutun2491 <yhosutun2491@gmail.com>
2022-11-06 11:48:17 +0800

    set StepperBars RWD 375px 1200px

--- src/component/main/RegistForm/FormStepper/StepperBars/StepperBars.js
+++ src/component/main/RegistForm/FormStepper/StepperBars/StepperBars.js
@@ -3,15 +3,6 @@ import completeLogo from '../../../../icons/pg-complete.svg'
 const StepperBarStyle = styled.div`
 margin: 0;
 position: relative ;
-  &:not(:last-child)::after {
-    content: '';
-    top: 50%;
-    left: calc(100% + 32px) ;
-    position: absolute ;
-    width: 60px ;
-    height: 2px ;
-    background-color: ${props => props.active ? 'black' : 'lightgrey'};
-  }
 .progress-group {
   display: flex ;
   font-size: 1.2rem ;
@@ -36,18 +27,42 @@ position: relative ;
   }
   & .progress-label {
     width: 80% ;
-    text-align:center ;
-    color: ${props => (props.active ? 'black' : 'grey')};
+    display: none ;
   }
   & .complete-logo {
     display:${props => props.step > props.stepNumber ? 'block' : 'none'};
   }
 }
+/*RWD mobile first/
+/*大於 375px 進度條才出現label*/
+@media (min-width: 375px) {
+  /*注意優先級問題*/
+  .progress-group { 
+    & .progress-label {
+    display: block;
+    text-align:center ;
+    color: ${props => (props.active ? 'black' : 'grey')};
+  }
+  }
+}
+
+ /*大於1200px才出現進度條*/
+@media (min-width: 1200px) {
+  &:not(:last-child)::after {
+    content: '';
+    top: 50%;
+    left: calc(100% + 32px) ;
+    position: absolute ;
+    width: 60px ;
+    height: 2px ;
+    background-color: ${props => props.active ? 'black' : 'lightgrey'};
+  }
+} 
 
 `
 // stpper 變換邏輯主要要父層state傳進來的參數來判斷，再把它傳入style-component做變化
 // props.stepNumber是元件本身的號碼、props.step是目前頁面狀態
-function StepperBars (props) {
+function StepperBars(props) {
   return (
     <StepperBarStyle active={props.active} step={props.step} stepNumber={props.stepNumber}>
       <span className='progress-group'>
