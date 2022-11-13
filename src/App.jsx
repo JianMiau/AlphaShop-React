8ec3ab8f4dd93b4524923e3925c635b15ed53bb0
yhosutun2491 <yhosutun2491@gmail.com>
2022-11-13 18:03:11 +0800

    rafactor App.js to .jsx and rafactor AppStyleWrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,20 @@ import { useState } from 'react'
 import Header from './component/Header'
 import Main from './component/Main'
 import Footer from './component/Footer'
-import styled from 'styled-components'
+import AppStyleWrap from './AppStyleWrap'
 import ResetStyle from './component/Styles/GlobalStyle/ResetStyle'
-// 使用styled-components
-const AppStyle = styled.div`
-  .header {
-    width: 100% ;
-    height: 60px ;
-    box-sizing: inherit;
-  }
-  .main {
-    width: 100% ;
-    height: auto ;
-    box-sizing: inherit;
-  }
-  .footer {
-    width: 100% ;
-  }
-`
+
 //規格要求在App.js設置useState控制表單
 function App() {
   // setFunction 命名記得用前面變數加上set
   const [pageStep, setPageStep] = useState(1)
   return (
-    <AppStyle>
+    <AppStyleWrap>
       <ResetStyle />
       <Header />
       <Main step={pageStep} onClick={setPageStep} />
       <Footer />
-    </AppStyle>
+    </AppStyleWrap>
   )
 }
 
---
+++ src/AppStyleWrap.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+const AppStyleWrap = styled.div`
+  .header {
+    width: 100% ;
+    height: 60px ;
+    box-sizing: inherit;
+  }
+  .main {
+    width: 100% ;
+    height: auto ;
+    box-sizing: inherit;
+  }
+  .footer {
+    width: 100% ;
+  }
+`
+export default AppStyleWrap
