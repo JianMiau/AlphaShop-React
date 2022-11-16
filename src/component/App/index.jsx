06f119d13a6c60840bfa9c19671632e479106451
yhosutun2491 <yhosutun2491@gmail.com>
2022-11-16 15:52:48 +0800

    CartContext.js basic design ok

--- src/component/App/index.jsx
+++ src/component/App/index.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
 import Header from '../Header'
 import Main from '../Main'
 import Footer from '../Footer'
 import AppStyleWrap from './AppStyleWrap'
 import ResetStyle from '../Styles/GlobalStyle/ResetStyle'
 import { FormProvider } from '../Context/FormContext'
+import { CartProvider } from '../Context/CartContext'
 
-//規格要求在App.js設置useState控制表單
 function App() {
-  // setFunction 命名記得用前面變數加上set
-  const [pageStep, setPageStep] = useState(1)
   return (
     <AppStyleWrap>
       <ResetStyle />
       <FormProvider>
-        <Header />
-        <Main step={pageStep} onClick={setPageStep} />
-        <Footer />
+        <CartProvider>
+          <Header />
+          <Main />
+          <Footer />
+        </CartProvider>
       </FormProvider>
     </AppStyleWrap>
   )
