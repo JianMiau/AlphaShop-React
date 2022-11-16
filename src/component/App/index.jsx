a5f05edb39541390a468cc73eab0098c4907c678
yhosutun2491 <yhosutun2491@gmail.com>
2022-11-16 08:18:58 +0800

    move App.js file to component file and refator name to index.js

--- src/component/App/index.jsx
+++ src/component/App/index.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import Header from './component/Header'
-import Main from './component/Main'
-import Footer from './component/Footer'
+import Header from '../Header'
+import Main from '../Main'
+import Footer from '../Footer'
 import AppStyleWrap from './AppStyleWrap'
-import ResetStyle from './component/Styles/GlobalStyle/ResetStyle'
+import ResetStyle from '../Styles/GlobalStyle/ResetStyle'
 
 //規格要求在App.js設置useState控制表單
 function App() {
