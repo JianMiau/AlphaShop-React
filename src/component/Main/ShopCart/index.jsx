196259f227e9d3b1a4b3dc92d44957ef29ee09c5
yhosutun2491 <yhosutun2491@gmail.com>
2022-11-16 18:21:16 +0800

    shiiping price can be alternate free or 500(DHL)

--- src/component/Main/ShopCart/index.jsx
+++ src/component/Main/ShopCart/index.jsx
@@ -2,11 +2,13 @@
 import Product from './Product'
 import ShopCartStyle from './ShopCartStyle'
 import { useCartData, useCartDispatch } from '@/component/Context/CartContext'
+import { useFormData } from '@/component/Context/FormContext'
 
 function ShopCart() {
   const cartData = useCartData()
   const dispatch = useCartDispatch()
   const totalPrice = cartData.find(item => item.id === 'total').totalPrice
+  const shippingType = useFormData().form[2].shipping
   // 按紐onClick事件handler (加減事件合併)
   function handleOnClick(productId, event) {
     //如果按到plus按鈕,要先取外層才抓得到data-set
@@ -30,7 +32,7 @@ function ShopCart() {
         </section>
         <section className='cart-info shipping'>
           <div className='text'>運費</div>
-          <div className='price'>免費</div>
+          <div className='price'>{shippingType === 'standard' ? '免費' : '$500'}</div>
         </section>
         <section className='cart-info total'>
           <div className='text'>小計</div>
